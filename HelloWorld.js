ef783eb1d2cf5e17b10efe0c745b4aff6e39e805
Rashed Miya <rashedmiyaofficial@gmail.com>
2026-03-28 09:51:35 +0600

    add

---
+++ HelloWorld.js
@@ -0,0 +1,8 @@
+function sum (a, b){
+    const sum = a + b;
+    console.log('Sum is: ', sum);
+}
+
+sum(2, 3); 
+
+
---
+++ SumOf2Numbers.js
@@ -0,0 +1,19 @@
+function sum(a, b){
+    const sum = a + b;
+    console.log('Sum is: ', sum);
+}
+
+sum(2, 3);
+sum(123, 234);
+sum(-1, 2);
+sum(1, 0);
+sum(1, 1.5);
+sum(2.5, 2.5); 
+
+
+
+
+
+
+
+
